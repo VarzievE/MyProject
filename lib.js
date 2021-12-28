dc5f267a8146759bfe88dc1b3429aa4d4a43d1d6
root <root@ITIC_ADMIN.mav.local>
2021-12-28 19:09:47 +0300

    Create slider 1: add plugin

--- lib.js
+++ lib.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
+
   
-    $(".header__fasearch, .header__search i").click(function(){
-      $(".header__search, .header__input, .header__search i").toggleClass("active");
-      $(".header__input[type='text']").focus();
-    });
+  $(".header__fasearch, .header__search i").click(function(){
+    $(".header__search, .header__input, .header__search i").toggleClass("active");
+    $(".header__input[type='text']").focus();
   });
+
+  /* SWIPER-SLIDER */
+  const swiper = new Swiper('.slider', {
+    
+  });
+});
